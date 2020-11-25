3e54263d3081e3beaf92381f16e9c29fc501c7f2
Bernard <bh20@hw.ac.uk>
2020-11-25 12:48:17 +0000

    Created atmel projects for controller and board

---
+++ Board/Board/main.c
@@ -0,0 +1,18 @@
+/*
+ * Board.c
+ *
+ * Created: 25/11/2020 12:47:52
+ * Author : Bernie
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ Controller/Controller/main.c
@@ -0,0 +1,18 @@
+/*
+ * Controller.c
+ *
+ * Created: 25/11/2020 12:47:18
+ * Author : Bernie
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
